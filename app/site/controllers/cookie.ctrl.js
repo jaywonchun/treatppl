27655dcf0743b5b31b1514165697110b8dee73f3
Justin Estaris <JustinEstaris@users.noreply.github.com>
2016-07-21 09:10:08 -0400

    Added 4 partials (I,C,C) & Controllers respectively

---
+++ app/site/controllers/cookie.ctrl.js
@@ -0,0 +1,30 @@
+(function(){
+	angular
+		.module('shopApp')
+		.controller('CookieCtrl',CookieCtrl);
+
+	function CookieCtrl($scope,productSrv){
+		var shopVm = this;
+
+		//TODO #3 Capture resolved products for view
+		// Get array of products from service and assign it
+		shopVm.products = productSrv.products;
+
+
+		// Function to retrieve all the products using the products service
+		shopVm.getAllProducts = getAllProducts;
+		function getAllProducts(){
+				// Get array and store it in local variable
+				var allProducts = productSrv.products;
+				console.log("allProducts",allProducts);
+		};
+
+		//watch for any changes to model data
+		$scope.$watch(function(){
+	    	return productSrv.products;
+		}, function (newValue) {
+		    shopVm.products = productSrv.products;
+		});
+	}
+
+})();
